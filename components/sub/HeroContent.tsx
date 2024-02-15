8ca32e3679e4374ec0238a57d08c35644dedda44
Shivansh Singh <singhshivansh12may@gmail.com>
2024-02-15 17:33:10 +0530

    Name Changes

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -33,12 +33,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            Hello I&apos;m{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Shivansh Singh{" "}
             </span>
-            project exprience
+            
           </span>
         </motion.div>
 
@@ -46,8 +46,8 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m a Full Stack Software Engineer with experience in Website
+           and Software development. Check out my projects and skills.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
