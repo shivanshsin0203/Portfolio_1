1de066d0a6aa99539b6b5aff63b1cdc456d7f9a9
Shivansh Singh <singhshivansh12may@gmail.com>
2024-02-16 10:59:57 +0530

    Added files

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,18 +12,18 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
+          src="/shoesale.png"
+          title="Shoe Sale Ecommerce"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
+          src="/creedo.png"
+          title="Creedo Social Network"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
+          src="/bytebond.png"
+          title="Bytebond Developer Community"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
       </div>
